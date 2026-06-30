868b814ebdd11c59fbdb867933ac37b82fa8f449
HanNajib <arroyhan27@gmail.com>
2026-06-30 16:39:40 +0700

    feat: CRUD Categories

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::prefix('admin')->group(function () {
+        Route::resource('categories', CategoriesController::class)->names('categories');
+    });
 });
 
-require __DIR__.'/settings.php';
+
+
+require __DIR__ . '/settings.php';
